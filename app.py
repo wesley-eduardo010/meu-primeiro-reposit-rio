e2b8547b4cde74428fb7c433d35895a92c2b4255
ws <wesley.e.souza@aluno.senai.br>
2025-06-04 09:03:33 -0300

    mudança no arquivo app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ def saudacao(nome: str) -> str:
     Retorna uma saudação simples para o nome informado.
     Exemplo: saudacao("Maria") -> "Olá, Maria!"
     """
-    return f"Olá, {nome}!"
+    return f"Olá, {nome},  bom dia !"
 
 if __name__ == "__main__":
     nome = input("Digite seu nome: ")
